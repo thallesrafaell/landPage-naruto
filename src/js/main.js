b45f7bbf9bdba5fee5fee4cc6049a9eba20136cd
Thalles Rafael <thallesrafaell@outlook.com>
2023-10-18 23:35:19 -0300

    ADD SECTION WALPAPERS E MEDIA QUERY

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
     const perguntas = document.querySelectorAll('[data-curiosidade-perguntas]')
-    console.log(perguntas)
-
+    //curiosidades
     for(let i = 0; i < perguntas.length; i++){
         perguntas[i].addEventListener('click', abreFechafaq)
     }
+    //scroll
+    const wallpaper = document.querySelector('.wallpapers')
+    const alturawallpaper = wallpaper.clientHeight
+
+    window.addEventListener('scroll', function(){
+        console.log(alturawallpaper)
+        
+        const posicaoAtual = window.scrollY
+        
+        console.log(posicaoAtual)
+        if(posicaoAtual > alturawallpaper){
+            document.getElementById('menu').style.backgroundImage = 'linear-gradient(180deg, rgba(2,1,0,1) 4%, rgba(2,1,0,0.7315301120448179) 44%, rgba(2,1,0,0) 100%)'
+        }else{
+            document.getElementById('menu').style.backgroundImage = 'linear-gradient(180deg, rgba(2,1,0,0) 4%, rgba(2,1,0,0) 44%, rgba(2,1,0,0) 100%)'
+        }
+    })
 
 })
 
