afa84dadab7fec0281c7fe7ec36fc460cd7f0be1
Thalles Rafael <thallesrafaell@outlook.com>
2023-10-19 22:20:23 -0300

    CONFIGURANDO MEDIA QUERIES

--- src/js/main.js
+++ src/js/main.js
@@ -2,28 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const perguntas = document.querySelectorAll('[data-curiosidade-perguntas]')
     //curiosidades
     for(let i = 0; i < perguntas.length; i++){
-        perguntas[i].addEventListener('click', abreFechafaq)
+        perguntas[i].addEventListener('click', abreFechaCuriosdades)
     }
-    //scroll
-    const wallpaper = document.querySelector('.wallpapers')
-    const alturawallpaper = wallpaper.clientHeight
-
-    window.addEventListener('scroll', function(){
-        console.log(alturawallpaper)
-        
-        const posicaoAtual = window.scrollY
-        
-        console.log(posicaoAtual)
-        if(posicaoAtual > alturawallpaper){
-            document.getElementById('menu').style.backgroundImage = 'linear-gradient(180deg, rgba(2,1,0,1) 4%, rgba(2,1,0,0.7315301120448179) 44%, rgba(2,1,0,0) 100%)'
-        }else{
-            document.getElementById('menu').style.backgroundImage = 'linear-gradient(180deg, rgba(2,1,0,0) 4%, rgba(2,1,0,0) 44%, rgba(2,1,0,0) 100%)'
-        }
-    })
-
 })
 
-function abreFechafaq(elemento){
+
+function abreFechaCuriosdades(elemento){
     const classe = 'curiosidades__perguntas__curiosidade--ativa'
     const pai = elemento.target.parentNode
     console.log(pai)
